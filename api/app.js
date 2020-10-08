74f0909f39e091e1b099739915c7f38ea4a81871
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-10-08 18:23:27 +0200

    added menu router to app.js

--- api/app.js
+++ api/app.js
@@ -21,6 +21,7 @@ const swaggerUIOptions = {
 //###[  Routers ]###
 const indexRouter = require('./index/indexRouter');
 const profileRouter = require('./profile/profileRouter');
+const menurouter = require('./menu/menuRouter');
 
 const app = express();
 
@@ -49,6 +50,7 @@ app.use(cookieParser());
 // application routes
 app.use('/', indexRouter);
 app.use(['/profile', '/profiles'], profileRouter);
+app.use(['/menu', ['menus']], menurouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
